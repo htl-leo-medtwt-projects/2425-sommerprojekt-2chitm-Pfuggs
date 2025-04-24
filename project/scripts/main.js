a8d373d5e62ba369bc9badbc858336fb308b94ed
theodor.g <t.truetsch@gmail.com>
2025-04-24 14:34:02 +0200

    Started copying design from figma

---
+++ project/scripts/main.js
@@ -0,0 +1,4 @@
+document.getElementById("mapClosed").style.display = "";
+document.getElementById("hubClosed").style.display = "none";
+document.getElementById("mapOpen").style.display = "none";
+document.getElementById("hubOpen").style.display = "none";
