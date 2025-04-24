1edb69fc6fad2c695ab8479eeb734dca39a9698d
theodor.g <t.truetsch@gmail.com>
2025-04-24 14:54:14 +0200

    copied more stuff from figma, top nav almost done

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -1,4 +1,32 @@
-document.getElementById("mapClosed").style.display = "";
+document.getElementById("mapClosed").style.display = "flex";
 document.getElementById("hubClosed").style.display = "none";
 document.getElementById("mapOpen").style.display = "none";
 document.getElementById("hubOpen").style.display = "none";
+
+function openMap() {
+    document.getElementById("mapClosed").style.display = "none";
+    document.getElementById("hubClosed").style.display = "none";
+    document.getElementById("mapOpen").style.display = "inline-flex";
+    document.getElementById("hubOpen").style.display = "none";
+}
+
+function closeMap() {
+    document.getElementById("mapClosed").style.display = "flex";
+    document.getElementById("hubClosed").style.display = "none";
+    document.getElementById("mapOpen").style.display = "none";
+    document.getElementById("hubOpen").style.display = "none";
+}
+
+function openHub() {
+    document.getElementById("mapClosed").style.display = "none";
+    document.getElementById("hubClosed").style.display = "none";
+    document.getElementById("mapOpen").style.display = "none";
+    document.getElementById("hubOpen").style.display = "inline-flex";
+}
+
+function closeHub() {
+    document.getElementById("mapClosed").style.display = "none";
+    document.getElementById("hubClosed").style.display = "flex";
+    document.getElementById("mapOpen").style.display = "none";
+    document.getElementById("hubOpen").style.display = "none";
+}
