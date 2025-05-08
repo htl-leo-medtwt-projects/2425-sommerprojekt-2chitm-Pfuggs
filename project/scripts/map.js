b82fc99652bea42538d3bd70398b99c33b254a6d
theodor.g <t.truetsch@gmail.com>
2025-05-08 10:15:44 +0200

    added basic map functionality, spot markers and adding spots (prototype, not final!)

---
+++ project/scripts/map.js
@@ -0,0 +1,64 @@
+function loadSpots() {
+    let spots = JSON.parse(localStorage.getItem('spots'));
+    if (spots == '') {
+        localStorage.setItem('spots', []);
+        spots = [];
+    }
+    return spots;
+}
+
+/* Setup Map */
+var map = L.map('contentMap').setView([48.268258, 14.252061], 12);
+
+// Satellite imagery layer
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Imagery © Esri',
+    maxZoom: 19
+}).addTo(map);
+
+// Labels and country outlines layer
+L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Labels © Esri',
+    maxZoom: 19
+}).addTo(map);
+
+/* Initialize markers */
+function loadMarkers() {
+    let markers = [];
+    let spots = loadSpots();
+    if (spots != null) {
+        for (let i = 0; i < spots.length; i++) {
+            markers[i] = L.marker([spots[i].lat, spots[i].lon]).addTo(map);
+        }
+    }
+}
+
+loadMarkers();
+
+/* Popup to add spot */
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent(`<div onclick="addSpot(${e.latlng.lat}, ${e.latlng.lng})">Add Spot</div>`)
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
+
+/* Add spot */
+function addSpot(lat, lon) {
+    let spots = loadSpots();
+    console.log(spots);
+    if (spots == null) {
+        spots = [{'lat': lat, 'lon': lon}]
+    } else {
+        spots[spots.length] = {'lat': lat, 'lon': lon}
+    }
+    localStorage.setItem('spots', JSON.stringify(spots));
+    console.log(spots);
+
+    map.closePopup();
+    loadMarkers();
+}
